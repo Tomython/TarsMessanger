5222f23c0ef787f61cf5af90515874b51f484f23
Tomython <dmitrymacpro@gmail.com>
2025-12-13 18:59:48 +0300

    update: program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://0.0.0.0:80");
 builder.Services.AddSignalR();
 builder.Services.AddCors(options => options.AddDefaultPolicy(p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 var app = builder.Build();
