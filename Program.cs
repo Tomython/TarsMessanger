8ddff70660dd98f39d2d1e172cdf0858d4a08206
Tomython <dmitrymacpro@gmail.com>
2025-12-13 20:23:24 +0300

    fix: program site

--- Program.cs
+++ Program.cs
@@ -1,16 +1,18 @@
 using Microsoft.AspNetCore.SignalR;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://0.0.0.0:8080");  // Фикс порта
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 builder.Services.AddSignalR();
 builder.Services.AddCors(options => 
-    options.AddDefaultPolicy(p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+    options.AddDefaultPolicy(p => 
+        p.WithOrigins("http://192.168.0.150:5000")  // ← ТОЧНЫЙ URL
+         .AllowAnyMethod().AllowAnyHeader()
+         .AllowCredentials()));  // ← КРИТИЧНО
 
 var app = builder.Build();
-
 app.UseCors();
-app.UseStaticFiles();  // ← КРИТИЧНО для wwwroot
+app.UseStaticFiles();
 app.MapHub<ChatHub>("/chatHub");
-app.MapFallbackToFile("index.html");  // ← КРИТИЧНО для /
-
+app.MapFallbackToFile("index.html");
 app.Run();
+
