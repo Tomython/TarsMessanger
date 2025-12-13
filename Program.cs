2d11289bb46fe04e93600d69ac955f50c57f5064
Tomython <dmitrymacpro@gmail.com>
2025-12-13 21:57:57 +0300

    fix AddControllers()

--- Program.cs
+++ Program.cs
@@ -3,39 +3,30 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.AspNetCore.Mvc;  // ← ДОБАВИТЬ
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
-// PostgreSQL
+// Controllers + PostgreSQL
+builder.Services.AddControllers();  // ← ДОБАВИТЬ ЭТУ СТРОКУ
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql("Host=localhost;Port=5432;Database=messenger;Username=messenger;Password=SecurePass123"));
+    options.UseNpgsql("Host=host.docker.internal;Port=5432;Database=messenger;Username=messenger;Password=SecurePass123"));
 
-// JWT
+// JWT + остальное
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new()
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super-secret-key-min-32-chars-long")),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
+    .AddJwtBearer(options => { /* ... */ });
 
 builder.Services.AddAuthorization();
 builder.Services.AddSignalR();
-builder.Services.AddCors(options => 
-    options.AddDefaultPolicy(p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+builder.Services.AddCors(/* ... */);
 
 var app = builder.Build();
-
 app.UseCors();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseStaticFiles();
 app.MapHub<ChatHub>("/chatHub");
-app.MapControllers();  // API контроллеры
+app.MapControllers();  // Теперь работает!
 app.MapFallbackToFile("index.html");
 app.Run();
