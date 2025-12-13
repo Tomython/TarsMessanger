c41194d2bd653135f64d05f728f3d843f8764dc8
Tomython <dmitrymacpro@gmail.com>
2025-12-13 20:16:58 +0300

    fix static files + fallback

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.AspNetCore.SignalR;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://0.0.0.0:80");
+builder.WebHost.UseUrls("http://0.0.0.0:8080");  // Фикс порта
 builder.Services.AddSignalR();
-builder.Services.AddCors(options => options.AddDefaultPolicy(p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+builder.Services.AddCors(options => 
+    options.AddDefaultPolicy(p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
 var app = builder.Build();
+
 app.UseCors();
-app.UseStaticFiles();
+app.UseStaticFiles();  // ← КРИТИЧНО для wwwroot
 app.MapHub<ChatHub>("/chatHub");
+app.MapFallbackToFile("index.html");  // ← КРИТИЧНО для /
+
 app.Run();
