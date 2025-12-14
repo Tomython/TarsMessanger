98bcd539a72216c10773fe369405c8d9d265ae00
Tomython <dmitrymacpro@gmail.com>
2025-12-15 00:12:54 +0300

    full no-db mvp

--- Program.cs
+++ Program.cs
@@ -8,35 +8,24 @@ using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
-// Controllers + PostgreSQL
-builder.Services.AddControllers();  // ← ДОБАВИТЬ ЭТУ СТРОКУ
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    options.UseNpgsql("Host=host.docker.internal;Port=5433;Database=messenger;Username=messenger;Password=SecurePass123"));
-
-// JWT
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new()
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super-secret-key-min-32-chars-long")),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
-
-builder.Services.AddAuthorization();
+builder.Services.AddControllers();
 builder.Services.AddSignalR();
-builder.Services.AddCors(options => 
-    options.AddDefaultPolicy(p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials());
+});
 
 var app = builder.Build();
-app.UseCors();
-app.UseAuthentication();
-app.UseAuthorization();
+
+app.UseCors("AllowAll");
 app.UseStaticFiles();
+app.MapControllers();
 app.MapHub<ChatHub>("/chatHub");
-app.MapControllers();  // Теперь работает!
 app.MapFallbackToFile("index.html");
+
 app.Run();
